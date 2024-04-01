7942e8ab94cadc292637b547d867f72c1a2979fb
kylecwalina <kylecwalina@gmail.com>
2024-04-01 08:50:52 -0400

    Autobattler sort of working

--- battlescene.py
+++ battlescene.py
@@ -11,12 +11,14 @@ def draw_battle_positions(rects):
         if x.position is not None:
             rects[x] = pygame.Rect((x.position), (small_icon))  # location, size
             screen.blit(x.rect, rects[x])
-            get_text_box((x.attack, x.health), 30, (x.position[0]+80, x.position[1]), RED) #width then height
+            get_text_box(x.attack, 50, (x.position[0]+15, x.position[1]-20), RED, horiboxscale=float(0.7),vertboxscale=float(0.5)) #width then height
+            get_text_box(x.health, 50, (x.position[0] + 70, x.position[1] - 20), BLUE, horiboxscale=float(0.7),vertboxscale=float(0.5))  # width then height
     for x in gameunits.OpponentUnitList:
         if x.position is not None:
             rects[x] = pygame.Rect((x.position), (small_icon))  # location, size
             screen.blit(x.rect, rects[x])
-            get_text_box((x.attack, x.health), 30, (x.position[0]+80, x.position[1]), RED)
+            get_text_box((x.attack), 50, (x.position[0]+15, x.position[1]-20), RED, horiboxscale=float(0.7),vertboxscale=float(0.5))
+            get_text_box((x.health), 50, (x.position[0] + 70, x.position[1] - 20), BLUE, horiboxscale=float(0.7),vertboxscale=float(0.5))  # width then height
     return rects
 
 def tree_building_rects_and_images():
